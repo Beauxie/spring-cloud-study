89308b320a7aa41ca3ac2c5ac1594afcc39000e9
Beauxie <15258787085@163.com>
2018-07-01 15:20:09 +0800

    first commit

---
+++ microservice-consumer-movie-feign/readme.md
@@ -0,0 +1,7 @@
+# microservice-consumer-movie-feign
+
+> 简单的微服务示例,作为服务消费者，使用feign与服务提供者通信
+
+* 只有一个自定义的接口:根据id获取用户信息，调用微服务api获取用户信息
+* 集成了Eureka服务，可以自动注册到服务中心
+* 访问[http://localhost:8010/1](http://localhost:8010/1)
---
+++ microservice-consumer-movie-feign/src/main/java/com/beauxie/cloud/study/service/UserFeignClient.java
@@ -0,0 +1,19 @@
+package com.beauxie.cloud.study.service;
+
+import com.beauxie.cloud.study.entity.User;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 用户接口服务
+ *
+ * @author Beauxie
+ * @date 2018/6/19.
+ */
+@FeignClient(name = "microservice-provider-user")
+public interface UserFeignClient {
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    User findById(@PathVariable("id") Long id);
+}
